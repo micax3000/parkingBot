75aea3a5726c681636bc85e9a4dc1d5f142fcb23
micax3000 <milan.veljkovic0097@gmail.com>
2023-02-28 17:15:04 +0100

    location sharing button works

--- handlers.py
+++ handlers.py
@@ -1,10 +1,23 @@
+import telegram
+
 import services as s
-from telegram.ext import ContextTypes
-from telegram import Update
+from telegram.ext import ContextTypes,ConversationHandler
+from telegram import Update, ReplyKeyboardMarkup,ReplyKeyboardRemove, KeyboardButton
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Zdravo {update.effective_chat.first_name}')
+    #reply_keyboard = [["/parking", "Lokacija"]]
+
+    custom_keyboard = [[KeyboardButton('/parking'),KeyboardButton('lokacija',request_location=True)]]
+
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard,resize_keyboard=True, one_time_keyboard=False)
+    await context.bot.send_message(chat_id=update.effective_chat.id,text = '/parking - Slobodna mesta po parkingu\nPosalji svoju lokaciju ako zelis da pronadjes najblize parkiraliste sa slobodnim mestima',reply_markup=reply_markup)
+    # await update.message.reply_text("Parking bot",
+    #     reply_markup=ReplyKeyboardMarkup(
+    #         reply_keyboard,resize_keyboard=True, one_time_keyboard=False, input_field_placeholder="Izaberi komandu"
+    #     ),
+    # )
+    #await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Zdravo {update.effective_chat.first_name}')
 
 
 async def text(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -23,3 +36,11 @@ async def location(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def freeSlots(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id,
                                    text="Slobodna mesta po garazama:\n\n" + '\n'.join(s.findSlots()))
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    user = update.message.from_user
+    await update.message.reply_text(
+        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import logging
 import handlers as hl
 import services as s
-from telegram.ext import filters, ApplicationBuilder, CommandHandler, MessageHandler
+from telegram.ext import filters, ApplicationBuilder, CommandHandler, MessageHandler,ConversationHandler
 from api import telegram_api
+
+KOMANDA, STANJE, LOKACIJA = range(3)
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -13,11 +15,20 @@ if __name__ == '__main__':
     s.findSlots()
     application = ApplicationBuilder().token(telegram_api).build()
 
+    # conv_handler = ConversationHandler(
+    #     entry_points=[CommandHandler("start", hl.start)],
+    #     states={
+    #         STANJE: [hl.freeSlots],
+    #     },
+    #     fallbacks=[CommandHandler("cancel", hl.cancel)],
+    # )
     start_handler = CommandHandler('start', hl.start)
     parking_handler = CommandHandler('parking', hl.freeSlots)
-    text_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), hl.text)
+    text_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), hl.start)
     location_handler = MessageHandler(filters.LOCATION, hl.location)
 
+
+    #application.add_handler(conv_handler)
     application.add_handler(location_handler)
     application.add_handler(start_handler)
     application.add_handler(text_handler)
