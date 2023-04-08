3aee58588b2bce2ffec3743f3d52c401aba3e80e
micax3000 <milan.veljkovic0097@gmail.com>
2023-04-08 19:29:07 +0200

    simple logging added. Checking for empty destinations in calcDistances added.

--- handlers.py
+++ handlers.py
@@ -1,6 +1,7 @@
+import logging
 import telegram
 
-import services as s
+import services as serv
 from telegram.ext import ContextTypes
 from telegram import Update, KeyboardButton
 
@@ -22,14 +23,16 @@ async def text(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def location(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    parking_info = await s.calculateDistances(update.message.location.latitude, update.message.location.longitude)
+    logging.info(f'calculate distances run for (lat:{update.message.location.latitude},long:{update.message.location.longitude})')
+    parking_info = await serv.calculateDistances(update.message.location.latitude, update.message.location.longitude)
     await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   text=f'Najblizi parking je: {parking_info[0]}\n na adresi: {parking_info[1]}\n sa: {parking_info[2]} slobodnih mesta')
+                                   text=f'Najblizi parking je: {parking_info[0]}\nna adresi: {parking_info[1]}\nsa: {parking_info[2]} slobodnih mesta')
     parking_info = parking_info[3].split(',')
     await context.bot.send_location(chat_id=update.effective_chat.id, latitude=parking_info[0],
                                     longitude=parking_info[1])
 
 
 async def freeSlots(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logging.info('freeSlots run')
     await context.bot.send_message(chat_id=update.effective_chat.id,
-                                   text="Slobodna mesta po garazama:\n\n" + '\n'.join(s.findSlots()))
+                                   text="Slobodna mesta po garazama:\n\n" + '\n'.join([f'{item[1][0]}: {item[1][1]}' for item in serv.parking_slots_state.items()]))
